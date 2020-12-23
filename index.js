26fcbe74fce00b84b6af3ea5e6812a65f60f9bcf
Ben Foxall <benfoxall@gmail.com>
2020-12-23 14:57:44 +0000

    Website logger

---
+++ index.js
@@ -0,0 +1,11 @@
+const fetch = require("node-fetch");
+
+
+const lintIt = (address) => {
+    fetch(address).then(async res => {
+        const text = await res.text();
+        console.log(text);
+    });
+}
+
+lintIt("http://www.benjaminbenben.com");
