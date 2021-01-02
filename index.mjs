715822497d21cd3f2d661a207b3de45d215b23bb
Ben Foxall <benfoxall@gmail.com>
2021-01-02 03:43:54 +0000

    Run in parallel

--- index.mjs
+++ index.mjs
@@ -3,77 +3,90 @@
 import fetch from "node-fetch";
 import { parse } from "node-html-parser";
 import isAbsolute from "is-absolute-url";
-import join from 'url-join'
-import ora from 'ora'
+import join from "url-join";
+import ora from "ora";
 import UserAgent from "user-agents";
 import Headers from "fetch-headers";
 
 const checklink = async (url) => {
-    const userAgent = new UserAgent();
+  const userAgent = new UserAgent();
 
-    const headers = new Headers();
-    headers.set("USER-AGENT", userAgent.toString());
+  const headers = new Headers();
+  headers.set("USER-AGENT", userAgent.toString());
 
-    let res = await fetch(url, {method: 'HEAD', headers})
+  let res = await fetch(url, { method: "HEAD", headers });
 
-    if(res.status === 405) {
-        res = await fetch(url, {headers});
+  if (res.status === 405) {
+    res = await fetch(url, { headers });
+  }
+
+  return res.ok;
+};
+
+const parallel = async (_queue, n = 1) =>
+  new Promise((resolve) => {
+    const queue = _queue.slice(0);
+
+    const run = () => {
+      const next = queue.shift();
+      if (next) Promise.resolve(next()).then(run);
+      else if (--n === 0) resolve();
+    };
+
+    for (let i = 0; i < n; i++) {
+      run();
     }
-    
-    return res.ok
-}
+  });
 
+const lintIt = async (address) => {
+  const res = await fetch(address);
+  const text = await res.text();
+  const dom = parse(text);
 
-const lintIt = (address) => {
-    fetch(address).then(async res => {
-        const text = await res.text();
-        const dom = parse(text);
-        let invalid = 0;
-        let working = 0;
-        const notWorking = [];
+  let invalid = 0;
+  let working = 0;
+  const notWorking = [];
 
-        for(const el of dom.querySelectorAll("a")) {
-            let url = el.getAttribute("href");
+  const queue = Array.from(dom.querySelectorAll("a"), (el) => async () => {
+    let url = el.getAttribute("href");
 
-            
-            
-            if(!url) {
-                invalid++;
-            }
+    if (!url) {
+      invalid++;
+    }
 
-            if(!isAbsolute(url)) {
-                url = join(address,url);
-            } 
+    if (!isAbsolute(url)) {
+      url = join(address, url);
+    }
 
-            const spinner = ora(url).start();
-            
-            const check = await checklink(url);
+    const spinner = ora(url).start();
 
-            if(check) {
-                working++
+    const check = await checklink(url);
 
-                spinner.succeed()
-            } else {
-                notWorking.push(url);
+    if (check) {
+      working++;
 
-                spinner.fail()
-            }
-        }
+      spinner.succeed();
+    } else {
+      notWorking.push(url);
 
-        console.log("WORKING", working);
-        console.log("NOT WORKING", notWorking.length);
-        if(notWorking.length) {
-            console.log(notWorking);
-        }
-        console.log("INVALID", invalid);
-    });
-}
+      spinner.fail();
+    }
+  });
 
-const url = process.argv[2]
+  await parallel(queue, 10);
 
-if(url) {
-    lintIt(url)    
+  console.log("WORKING", working);
+  console.log("NOT WORKING", notWorking.length);
+  if (notWorking.length) {
+    console.log(notWorking);
+  }
+  console.log("INVALID", invalid);
+};
+
+const url = process.argv[2];
+
+if (url) {
+  lintIt(url);
 } else {
-    console.warn("usage: npx hyperlint http://yoursite.com")
+  console.warn("usage: npx hyperlint http://yoursite.com");
 }
-
